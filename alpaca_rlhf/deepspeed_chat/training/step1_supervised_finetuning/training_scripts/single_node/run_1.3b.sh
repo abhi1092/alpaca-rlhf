d75a23b759a386c3df350236ed9739209c3d0949
Abhishek Bhandwaldar <abhi1092@gmail.com>
2023-07-26 18:13:51 -0400

    Update run_1.3b.sh

--- alpaca_rlhf/deepspeed_chat/training/step1_supervised_finetuning/training_scripts/single_node/run_1.3b.sh
+++ alpaca_rlhf/deepspeed_chat/training/step1_supervised_finetuning/training_scripts/single_node/run_1.3b.sh
@@ -16,7 +16,7 @@ mkdir -p $OUTPUT
 deepspeed main.py \
    --data_path Dahoas/rm-static Dahoas/full-hh-rlhf Dahoas/synthetic-instruct-gptj-pairwise yitingxie/rlhf-reward-datasets \
    --data_split 2,4,4 \
-   --model_name_or_path facebook/opt-1.3b \
+   --model_name_or_path EleutherAI/pythia-2.8b-deduped \
    --per_device_train_batch_size 8 \
    --per_device_eval_batch_size 8 \
    --max_seq_len 512 \
